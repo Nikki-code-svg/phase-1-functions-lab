faaeaed18c9066e4fa92c00d3a07ae9cd7a75e2a
nicole wachter <nicole.wachter6@gmail.com>
2024-06-10 10:34:49 -0400

    Adds index.js

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
 // Code your solution in this file!
+ function distanceFromHqInBlocks(blocks){
+if (blocks > 42){
+    return blocks - 42;
+ } else {
+    return 42 - blocks;
+ }
+
+}
+function distanceFromHqInFeet(blocks) {
+   let feet = distanceFromHqInBlocks(blocks)
+   return feet * 264;
+   
+}
+
+ function distanceTravelledInFeet(start, distance){
+       if (start > distance){
+        return (start - distance) * 264;
+       } else {
+         return (distance - start) * 264;
+       }
+    }
+        
+
+ function calculatesFarePrice(start, destination) {
+    const feet = Math.abs((start - destination))* 264;
+    if(feet < 400){
+        return 0;
+    } else if (feet > 400 && feet < 2000) {
+        return (feet - 400) * .02;
+     } else if (feet > 2000 && feet < 2500){
+        return 25;
+      } else {
+        return "cannot travel that far";
+     }
+  
+ }
